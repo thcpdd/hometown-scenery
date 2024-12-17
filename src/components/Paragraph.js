47a3049c74d4fd5611ec0af3baf4990fd78199f3
unknown <1834763300@qq.com>
2024-12-17 19:20:26 +0800

    fix: 调整首页部分信息

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -1,8 +1,8 @@
 import AntdParagraph from "antd/es/typography/Paragraph";
 
 /*
-* 重写 Ant Design 的 Paragraph 组件
-* 重写后：字体大小默认 16px，每个组件自带 2 个字符的缩进
+* 拓展 Ant Design 的 Paragraph 组件
+* 拓展后：字体大小默认 16px，每个组件自带 2 个字符的缩进
 * */
 const Paragraph = ({ children, fontSize, ...args }) => {
   return (
