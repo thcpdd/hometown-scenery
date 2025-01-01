2b849fc21950a9e24e79b6b695ce2aebe356b1a9
unknown <1834763300@qq.com>
2025-01-01 13:29:16 +0800

    feat: 留言板基本完成

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,39 +1,78 @@
 'use client'
 
 import { useState } from "react";
-import { Divider, Modal, Input, Form, Select, Tag } from "antd";
+import { Divider, Modal, Input, Form, Select, Tag, Pagination, message } from "antd";
 import TextArea from "antd/es/input/TextArea";
 import Title from "antd/es/typography/Title";
 import styles from "@/styles/contact.module.css"
 
 
-const ContactContent = () => {
+const ContactContentItem = ({ username, phone, datetime, title, tag, content }) => {
   return (
       <div>
         <div style={{marginBottom: '5px'}}>
-          <span>Rainbow&nbsp;(19167820287)</span>
+          <span>{ username }</span><br/>
+          <span style={{color: 'gray', fontSize: '14px'}}>{ datetime }</span>
         </div>
         <div style={{display: 'flex', alignItems: 'center', marginBottom: '10px'}}>
-          <span style={{fontWeight: "bold", marginRight: '10px', fontSize: '19px'}}>标题</span>
-          <Tag color="#34b585">象鼻山</Tag>
+          <span style={{fontWeight: "bold", marginRight: '10px', fontSize: '20px'}}>{ title }</span>
+          <Tag color="#34b585">{ tag }</Tag>
         </div>
-        <div style={{color: 'gray'}}>内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容</div>
+        <div style={{color: 'gray'}}>{ content }</div>
+        <div style={{marginTop: '10px', display: 'flex'}}>
+          <span style={{fontSize: '15px', color: 'gray', marginLeft: 'auto'}}>——该留言来自手机尾号 { phone } 的游客</span>
+        </div>
         <Divider/>
       </div>
   );
 }
 
+const ContactContent = () => {
+  // 所有的留言内容实际情况应来自于后端
+  const contents = [
+    {username: 'Rainbow', phone: '0287', datetime: '2025年1月1日 10:37', title: '象鼻山太好堪啦！', tag: '象鼻山', content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容'},
+    {username: '游客1', phone: '1234', datetime: '2024年12月31日 10:38', title: '漓江，推荐', tag: '漓江', content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容'},
+    {username: '游客2', phone: '2345', datetime: '2024年12月30日 09:11', title: '桂林推荐景点', tag: '灵渠', content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容'},
+    {username: '游客3', phone: '3456', datetime: '2024年12月30日 08:56', title: '象鼻山留言', tag: '象鼻山', content: '内容内容内容内容内容内容内'},
+    {username: '游客4', phone: '4567', datetime: '2024年12月27日 18:37', title: '灵渠景点介绍', tag: '灵渠', content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容'},
+    {username: '游客5', phone: '5678', datetime: '2024年12月16日 10:25', title: '漓江景区心得', tag: '漓江', content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容'},
+    {username: '游客6', phone: '6789', datetime: '2024年12月13日 19:32', title: '欢迎来到桂林景点象鼻山', tag: '象鼻山', content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容'},
+  ]
+  const [current, setCurrent] = useState(1)
+  const pageSize = 5
+  const currentContent = contents.slice((current - 1) * pageSize, (current - 1) * pageSize + pageSize)
+  const ContactContentItems = currentContent.map((item, i) => {
+    return <ContactContentItem key={i} {...item}/>
+  })
+
+  return (
+      <div>
+        <div>{ContactContentItems}</div>
+        <Pagination
+            current={current}
+            onChange={v => {setCurrent(v)}}
+            total={contents.length}
+            defaultCurrent={1}
+            pageSize={pageSize}
+            align="center"
+        />
+      </div>
+  )
+}
+
 const Page = () => {
   const [openModal, setOpenModal] = useState(false)
+  const [messageApi, contextHolder] = message.useMessage();
   const selectOptions = [
     {label: '漓江', value: 'lijiang'},
     {label: '象鼻山', value: 'xiangbishan'},
     {label: '灵渠', value: 'lingqu'},
   ]
-  const onCancel = () => {
-    setOpenModal(false)
-  }
-  const publish = values => {
+  const onFinish = values => {
+    messageApi.open({
+      type: 'success',
+      content: '留言发表成功！'
+    })
     console.log(values)
     setOpenModal(false)
   }
@@ -41,11 +80,12 @@ const Page = () => {
   return (
       <div className={styles.content}>
         <div className={styles.title}>
-          <Title level={2} style={{textAlign: 'center'}}>留言板</Title>
-          <span className={styles.publish} onClick={() => {setOpenModal(true)}}>发表留言&nbsp;&gt;</span>
+          {contextHolder}
+          <Title level={2} style={{textAlign: 'center'}}>📒留言板</Title>
+          <span className={styles.publish} onClick={() => {setOpenModal(true)}}>发表留言</span>
           <Modal
               open={openModal}
-              onCancel={onCancel}
+              onCancel={() => {setOpenModal(false)}}
               title={"发表留言"}
               okText={"发表"}
               cancelText={"取消"}
@@ -58,7 +98,7 @@ const Page = () => {
                         modifier: 'public',
                       }}
                       clearOnDestroy
-                      onFinish={publish}
+                      onFinish={onFinish}
                   >{dom}</Form>
               )}
           >
@@ -68,7 +108,7 @@ const Page = () => {
             <Form.Item name="phone" label="手机号" rules={[{required: true, message: '请输入手机号'}]}>
               <Input/>
             </Form.Item>
-            <Form.Item name='scenery' label="留言景点" rules={[{required: true, message: '请选择需要留言的景点'}]}>
+            <Form.Item name='scenery' label="意向景点" rules={[{required: true, message: '请选择需要留言的景点'}]}>
               <Select options={selectOptions}/>
             </Form.Item>
             <Form.Item name="title" label="留言标题" rules={[{required: true, message: '请输入留言标题'}]}>
